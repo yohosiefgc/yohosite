570f984017a5631d961ac55c5dedd6d23c1b558f
Yohosie <dawnhosiedesign@gmail.com>
2022-06-26 21:54:27 -0700

    Desktop navigation 1.0 finished

--- scripts/script.js
+++ scripts/script.js
@@ -7,54 +7,115 @@ const navLi = document.querySelectorAll('.navigation-li');
 const navIconLineOne = document.querySelector('.sidebar-nav-icon-line-1');
 const navIconLineTwo = document.querySelector('.sidebar-nav-icon-line-2');
 const navIconLineBorder = document.querySelector('.sidebar-nav-icon-border');
+const navDiagonals = document.querySelector('.navigation-svg-container');
+
+const titleDefault = document.querySelector('.title-default');
+const titleHover = document.querySelector('.title-hover');
 
 let navToggleTracker = false; //TODO: I think this isn't right
+let buttonDisable = false; //TODO: I think this isn't right
+let leftNav = false;
 
-navToggle.addEventListener('mouseenter', function(){
-    if(navToggleTracker === true){ 
+
+//title hover
+titleDefault.addEventListener('mouseenter', function () {
+    titleHover.classList.remove('fade-in-start');
+    titleHover.classList.add('fade-in-anim');
+});
+
+titleDefault.addEventListener('mouseleave', function () {
+    titleHover.classList.add('fade-in-start');
+    titleHover.classList.remove('fade-in-anim');
+});
+
+//main navigation listener
+navToggle.addEventListener('mouseenter', function () {
+    if (!navToggleTracker && !buttonDisable) {
+        buttonDisable = true;
+        showNav();
+        setTimeout(function () {
+            buttonDisable = false;
+        }, 1000);
+    }
+    else {
+        console.log("standard nav close");
+        hideNavCheck();
+    }
+});
+
+// REMOVED
+// edgecase nav, if you hover back while the button is disabled, it will close after x seconds
+// navToggle.addEventListener('mousemove', function () {
+//     console.log('mousemove');
+//     hoverNavFallback();
+// });
+
+//makes it so that if you hover over the button while reading the main text, you must hover out to hoverNavFallback
+navToggle.addEventListener('mouseleave', function () {
+    leftNav = true;
+});
+
+// sidebar navigation listener
+for (let i = 0; i < navAltToggle.length; i++){
+    navAltToggle[i].addEventListener('mouseenter', function () { hideNavCheck(); });
+    navAltToggle[i].addEventListener('mouseover', function () { hoverNavFallback(); });
+}
+
+let hoverNavFallback = () => {
+    if (!buttonDisable && leftNav) {
+        console.log("fallback navigation initiated");
+        hideNavCheck();
+    }
+}
+
+let hideNavCheck = () => {
+    if (navToggleTracker && !buttonDisable) {
         hideNav()
     }
-    else{
-        showNav(); 
-    } //todo reduce to 1 line
-})
-
-navAltToggle[0].addEventListener('mouseenter', function(){ hideNav(); });
-navAltToggle[1].addEventListener('mouseenter', function(){ hideNav(); }); //TODO: Make simpler, combine
+    else {
+        console.log("can't hide nav rn");
+    }
+}
 
 const showNav = () => {
     navToggleTracker = true;
     nav.classList.remove('navigation-hide');
     nav.classList.add('navigation-show');
 
-    nav.classList.remove('navigation-animation-bg-fadein');
+    //bg fadein animation
+    nav.classList.remove('navigation-animation-bg-fadeout');
     nav.classList.add('navigation-animation-bg-fadein');
     nav.style.backgroundColor = ('rgba(0, 0, 0, .85)'); // ??? WHY?
+
+
 
     navIconLineOne.classList.add('svg1animation');
     navIconLineTwo.classList.add('svg1animation');
     navIconLineBorder.classList.add('svg2animation');
     setTimeout(function () {
         navMain.classList.add('navigation-animation');
+        navDiagonals.classList.add('diagonal-rectangle-navigation');
         for (let i = 0; i < navLi.length; i++){
             navLi[i].classList.add('navigation-li-animation');
         }
     }, 10);
-    // navIconLineOne.classList.add('active');
-    // navIconLineTwo.classList.add('active'); //TODO: I can remove this I think
 }
 
 const hideNav = () => {
     navToggleTracker = false;
+    leftNav = false; //sets fallback to false
 
+    //bg fadeout animation
     nav.classList.remove('navigation-animation-bg-fadein');
     nav.classList.add('navigation-animation-bg-fadeout');
-    nav.style.backgroundColor = ('rgba(0, 0, 0, 0)');
+    nav.style.backgroundColor = ('rgba(0, 0, 0, 0)'); // ??? WHY?
 
     navIconLineTwo.classList.remove('svg1animation');
     navIconLineOne.classList.remove('svg1animation');
     navIconLineBorder.classList.remove('svg2animation');
     navMain.classList.remove('navigation-animation');
+    navDiagonals.classList.remove('diagonal-rectangle-navigation');
+
     for (let i = 0; i < navLi.length; i++){
         navLi[i].classList.remove('navigation-li-animation');
     }
