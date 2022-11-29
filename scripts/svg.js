4f583ddc774557a248a3d20fa62772d6c534c2a2
Yohosie <dawnhosiedesign@gmail.com>
2022-11-29 10:09:05 -0800

    Fixed snapScrolling

--- scripts/svg.js
+++ scripts/svg.js
@@ -1,85 +1,108 @@
 document.write(`<svg id="svg-storage">
-<defs>
-  <polygon id="triangle" data-name="triangle" points="0,0 0,500 500,500" fill="#FFF" />
-</defs>
-<defs>
-  <rect id="rectangle" data-name="rectangle" style="width:100%; height:100%;">
-</defs>
-<defs>
-  <path id="arrow" data-name="arrow" d="
-    M 21.52, 28.94
-    H 10.61
-    L 21.22, 18.33
-    H 0
-    V 10.61
-    H 21.22
-    L 10.61, 0
-    H 21.52
-    L 36, 14.47
-    Z"/>
-</defs>
-<defs>
-<defs>
-  <g id="end-page">
-    <path d="M42.96,57.79H21.19l21.17-21.19H0v-15.41H42.36L21.19,0h21.78l28.88,28.9-28.88,28.9Z"/>
-    <path d="M85.78,29.15L56.85,0h226.77l28.88,28.9-28.88,28.9H56.85l28.94-28.64Z"/>
-  </g>
-</defs>
-</defs>
-<defs>
-  <pattern id="pattern-line-diagonal-thin" width="6" height="6" patternUnits="userSpaceOnUse" patternContentUnits="userSpaceOnUse">
-    <g>
-      <line x1="0" y1="0" x2="6" y2="6" stroke="white" stroke-width="1px"></line>
-      <line x1="-1" y1="5" x2="6" y2="12" stroke="white" stroke-width="1px"></line>
-      <line x1="5" y1="-1" x2="12" y2="6" stroke="white" stroke-width="1px"></line>
+  <defs>
+    <polygon id="triangle" data-name="triangle" points="0,0 0,500 500,500" fill="#FFF" />
+  </defs>
+  <defs>
+    <rect id="rectangle" data-name="rectangle" style="width:100%; height:100%;">
+  </defs>
+  <defs>
+    <path id="arrow" data-name="arrow" d="
+      M 21.52, 28.94
+      H 10.61
+      L 21.22, 18.33
+      H 0
+      V 10.61
+      H 21.22
+      L 10.61, 0
+      H 21.52
+      L 36, 14.47
+      Z"/>
+  </defs>
+  <defs>
+  <defs>
+    <g id="end-page">
+      <path d="M42.96,57.79H21.19l21.17-21.19H0v-15.41H42.36L21.19,0h21.78l28.88,28.9-28.88,28.9Z"/>
+      <path d="M85.78,29.15L56.85,0h226.77l28.88,28.9-28.88,28.9H56.85l28.94-28.64Z"/>
     </g>
-  </pattern>
-  <pattern id="pattern-line-diagonal" width="10" height="10" patternUnits="userSpaceOnUse" patternContentUnits="userSpaceOnUse">
-    <g>
-      <line x1="0" y1="0" x2="10" y2="10" stroke="white" stroke-width="1px"></line>
-      <line x1="-1" y1="9" x2="10" y2="20" stroke="white" stroke-width="1px"></line>
-      <line x1="9" y1="-1" x2="20" y2="10" stroke="white" stroke-width="1px"></line>
+  </defs>
+  </defs>
+  <defs>
+    <pattern id="pattern-line-diagonal-thin" width="6" height="6" patternUnits="userSpaceOnUse" patternContentUnits="userSpaceOnUse">
+      <g>
+        <line x1="0" y1="0" x2="6" y2="6" stroke="white" stroke-width="1px"></line>
+        <line x1="-1" y1="5" x2="6" y2="12" stroke="white" stroke-width="1px"></line>
+        <line x1="5" y1="-1" x2="12" y2="6" stroke="white" stroke-width="1px"></line>
+      </g>
+    </pattern>
+    <pattern id="pattern-line-diagonal" width="10" height="10" patternUnits="userSpaceOnUse" patternContentUnits="userSpaceOnUse">
+      <g>
+        <line x1="0" y1="0" x2="10" y2="10" stroke="white" stroke-width="1px"></line>
+        <line x1="-1" y1="9" x2="10" y2="20" stroke="white" stroke-width="1px"></line>
+        <line x1="9" y1="-1" x2="20" y2="10" stroke="white" stroke-width="1px"></line>
+      </g>
+    </pattern>
+    <pattern id="pattern-line-diagonal-thick" width="10" height="10" patternUnits="userSpaceOnUse" patternContentUnits="userSpaceOnUse">
+      <g>
+        <line x1="0" y1="0" x2="10" y2="10" stroke="white" stroke-width="3px"></line>
+        <line x1="-1" y1="9" x2="10" y2="20" stroke="white" stroke-width="3px"></line>
+        <line x1="9" y1="-1" x2="20" y2="10" stroke="white" stroke-width="3px"></line>
+      </g>
+    </pattern>
+    <pattern id="pattern-line-diagonal-wide" width="20" height="20" patternUnits="userSpaceOnUse" patternContentUnits="userSpaceOnUse">
+      <g>
+        <line x1="0" y1="0" x2="20" y2="20" stroke="white" stroke-width="2px"></line>
+        <line x1="-1" y1="19" x2="20" y2="40" stroke="white" stroke-width="2px"></line>
+        <line x1="19" y1="-1" x2="40" y2="20" stroke="white" stroke-width="2px"></line>
+      </g>
+    </pattern>
+  </defs>
+  <defs>
+    <pattern id="pattern-line-horizontal" width="10" height="5" patternUnits="userSpaceOnUse" patternContentUnits="userSpaceOnUse">
+      <line x1="0" y1="0" x2="10" y2="0" stroke="white" stroke-width="2px"></line>
+    </pattern>
+    <pattern id="pattern-line-horizontal-thick" width="10" height="5" patternUnits="userSpaceOnUse" patternContentUnits="userSpaceOnUse">
+      <line x1="0" y1="0" x2="10" y2="0" stroke="white" stroke-width="4px"></line>
+    </pattern>
+  </defs>
+  <defs>
+    <line id="line-dashed-horizontal" x1="0" y1="0" x2="10000" y2="0" stroke="white" stroke-width="5px" stroke-dasharray="15,7"></line>
+  </defs>
+  <defs>
+    <line id="portfolio-end-diagonal" x1="0" y1="0" x2="6600" y2="10000" stroke="white" stroke-width="1px"></line>
+  </defs>
+  <defs>
+    <g id="zero-one" data-name="zero-one">
+        <path class="st0" d="M0,8.88C0,3.94,3.87,0,9.08,0c5.28,0,9.22,3.94,9.22,8.88v47.32c0,4.9-3.94,8.81-9.22,8.81
+          C3.87,65,0,61.09,0,56.19V8.88z M9.08,64.04c4.56,0,7.95-3.5,7.95-7.85V8.88c0-4.39-3.39-7.92-7.95-7.92
+          c-4.49,0-7.81,3.53-7.81,7.92v47.32C1.27,60.54,4.59,64.04,9.08,64.04z M3.67,8.98c0-2.95,2.19-5.24,5.41-5.24
+          c3.29,0,5.48,2.3,5.48,5.24v47.11c0,2.95-2.19,5.17-5.48,5.17c-3.22,0-5.41-2.23-5.41-5.17V8.98z M9.08,60.3
+          c2.54,0,4.22-1.82,4.22-4.22V8.98c0-2.4-1.68-4.28-4.22-4.28c-2.47,0-4.15,1.88-4.15,4.28v47.11C4.94,58.49,6.61,60.3,9.08,60.3z"
+          />
+        <path class="st0" d="M23.85,7.47l-1.34,1.3l-2.61-2.98l4.8-5.11h4.22v63.63h-5.07V7.47z M27.66,63.36V1.64h-2.3l-3.94,4.15
+          l1.17,1.34l2.54-2.5v58.73H27.66z"/>
     </g>
-  </pattern>
-  <pattern id="pattern-line-diagonal-thick" width="10" height="10" patternUnits="userSpaceOnUse" patternContentUnits="userSpaceOnUse">
-    <g>
-      <line x1="0" y1="0" x2="10" y2="10" stroke="white" stroke-width="3px"></line>
-      <line x1="-1" y1="9" x2="10" y2="20" stroke="white" stroke-width="3px"></line>
-      <line x1="9" y1="-1" x2="20" y2="10" stroke="white" stroke-width="3px"></line>
+  <defs>
+    <g id="zero-two" data-name="zero-two">
+      <path class="cls-1" d="M0,8.88C0,3.94,3.87,0,9.08,0s9.22,3.94,9.22,8.88V56.19c0,4.9-3.94,8.81-9.22,8.81S0,61.09,0,56.19V8.88Zm9.08,55.16c4.56,0,7.95-3.5,7.95-7.85V8.88c0-4.39-3.39-7.92-7.95-7.92S1.27,4.49,1.27,8.88V56.19c0,4.35,3.32,7.85,7.81,7.85ZM3.67,8.98c0-2.95,2.19-5.24,5.41-5.24s5.48,2.3,5.48,5.24V56.09c0,2.95-2.19,5.17-5.48,5.17s-5.41-2.23-5.41-5.17V8.98Zm5.41,51.33c2.54,0,4.22-1.82,4.22-4.22V8.98c0-2.4-1.68-4.28-4.22-4.28s-4.15,1.88-4.15,4.28V56.09c0,2.4,1.68,4.22,4.15,4.22Z"/>
+      <path class="cls-1" d="M31.47,7.03c.1-1.14-.87-2.16-2.18-2.14-1.69,0-3.03,1.27-3.03,2.85v1.07h-5.23l.05-1.07c0-4.23,3.66-7.75,8.21-7.75s7.1,3.52,7.1,7.75c0,2.11-9.24,52.05-9.24,52.05h10.24v4.61H19.92v-.56L30.64,12.25s.68-3.41,.83-5.21Zm-6.44,.71c0-2.15,1.83-3.96,4.26-3.91,2.02,.04,3.26,1.55,3.44,2.48s-.26,3.16-.26,3.16l-10.58,53.95h14.19v-2.64h-11.01v-.53S35.09,9.68,35.09,7.75c0-3.7-2.03-6.76-5.8-6.76s-6.94,3.06-6.94,6.76"/>
     </g>
-  </pattern>
-  <pattern id="pattern-line-diagonal-wide" width="20" height="20" patternUnits="userSpaceOnUse" patternContentUnits="userSpaceOnUse">
-    <g>
-      <line x1="0" y1="0" x2="20" y2="20" stroke="white" stroke-width="2px"></line>
-      <line x1="-1" y1="19" x2="20" y2="40" stroke="white" stroke-width="2px"></line>
-      <line x1="19" y1="-1" x2="40" y2="20" stroke="white" stroke-width="2px"></line>
+  </defs>
+  <defs>
+    <g id="zero-three" data-name="zero-three">
+      <path class="cls-1" d="M0,8.92C0,3.99,3.87,.05,9.08,.05s9.22,3.94,9.22,8.88V56.24c0,4.9-3.94,8.81-9.22,8.81S0,61.14,0,56.24V8.92Zm9.08,55.16c4.56,0,7.95-3.5,7.95-7.85V8.92c0-4.39-3.39-7.92-7.95-7.92S1.27,4.54,1.27,8.92V56.24c0,4.35,3.32,7.85,7.81,7.85ZM3.67,9.03c0-2.95,2.19-5.24,5.41-5.24s5.48,2.3,5.48,5.24V56.14c0,2.95-2.19,5.17-5.48,5.17s-5.41-2.23-5.41-5.17V9.03Zm5.41,51.33c2.54,0,4.22-1.82,4.22-4.22V9.03c0-2.4-1.68-4.28-4.22-4.28s-4.15,1.88-4.15,4.28V56.14c0,2.4,1.68,4.22,4.15,4.22Z"/>
+      <path class="cls-1" d="M36.51,18.44c2.11-1.92,3.46-4.78,3.46-7.97,0-5.77-4.4-10.47-9.8-10.47s-9.8,4.7-9.8,10.47v2h4.68v-2c0-3.01,2.3-5.47,5.12-5.47s5.12,2.45,5.12,5.47-2.3,5.47-5.12,5.47h-5.59v5h5.59c2.82,0,5.12,2.45,5.12,5.47v28.22c0,3.01-2.3,5.47-5.12,5.47s-5.12-2.45-5.12-5.47v-2h-4.68v2c0,5.77,4.4,10.47,9.8,10.47s9.8-4.7,9.8-10.47V26.4c0-3.19-1.35-6.05-3.46-7.97Zm2.21,36.19c0,5.04-3.84,9.13-8.55,9.13s-8.55-4.1-8.55-9.13v-.67h2.18v.67c0,3.75,2.86,6.8,6.37,6.8s6.37-3.05,6.37-6.8V26.4c0-2.58-1.35-4.83-3.34-5.98-.9-.52-1.93-.82-3.03-.82h-4.34v-2.33h4.34c.35,0,.68,.03,1.02,.07,.79-.13,1.53-.41,2.19-.82,1.97-1.2,3.31-3.46,3.31-6.06,0-3.84-2.93-6.97-6.52-6.97s-6.52,3.13-6.52,6.97v.5h-1.87v-.5c0-4.94,3.77-8.97,8.4-8.97s8.4,4.02,8.4,8.97c0,2.86-1.27,5.41-3.23,7.06-.36,.3-.75,.58-1.15,.82,.46,.26,.89,.57,1.29,.91,1.97,1.67,3.24,4.25,3.24,7.15v28.22Z"/>
     </g>
-  </pattern>
-</defs>
-<defs>
-  <pattern id="pattern-line-horizontal" width="10" height="5" patternUnits="userSpaceOnUse" patternContentUnits="userSpaceOnUse">
-    <line x1="0" y1="0" x2="10" y2="0" stroke="white" stroke-width="2px"></line>
-  </pattern>
-  <pattern id="pattern-line-horizontal-thick" width="10" height="5" patternUnits="userSpaceOnUse" patternContentUnits="userSpaceOnUse">
-    <line x1="0" y1="0" x2="10" y2="0" stroke="white" stroke-width="4px"></line>
-  </pattern>
-</defs>
-<defs>
-  <line id="line-dashed-horizontal" x1="0" y1="0" x2="10000" y2="0" stroke="white" stroke-width="5px" stroke-dasharray="15,7"></line>
-</defs>
-<defs>
-  <line id="portfolio-end-diagonal" x1="0" y1="0" x2="6600" y2="10000" stroke="white" stroke-width="1px"></line>
-</defs>
-<defs>
-  <g id="zero-one" data-name="zero-one">
-      <path class="st0" d="M0,8.88C0,3.94,3.87,0,9.08,0c5.28,0,9.22,3.94,9.22,8.88v47.32c0,4.9-3.94,8.81-9.22,8.81
-        C3.87,65,0,61.09,0,56.19V8.88z M9.08,64.04c4.56,0,7.95-3.5,7.95-7.85V8.88c0-4.39-3.39-7.92-7.95-7.92
-        c-4.49,0-7.81,3.53-7.81,7.92v47.32C1.27,60.54,4.59,64.04,9.08,64.04z M3.67,8.98c0-2.95,2.19-5.24,5.41-5.24
-        c3.29,0,5.48,2.3,5.48,5.24v47.11c0,2.95-2.19,5.17-5.48,5.17c-3.22,0-5.41-2.23-5.41-5.17V8.98z M9.08,60.3
-        c2.54,0,4.22-1.82,4.22-4.22V8.98c0-2.4-1.68-4.28-4.22-4.28c-2.47,0-4.15,1.88-4.15,4.28v47.11C4.94,58.49,6.61,60.3,9.08,60.3z"
-        />
-      <path class="st0" d="M23.85,7.47l-1.34,1.3l-2.61-2.98l4.8-5.11h4.22v63.63h-5.07V7.47z M27.66,63.36V1.64h-2.3l-3.94,4.15
-        l1.17,1.34l2.54-2.5v58.73H27.66z"/>
-  </g>
-</defs>
+  </defs>
+  <defs>
+    <g id="zero-four" data-name="zero-four">
+      <path class="cls-1" d="M0,8.92C0,3.99,3.87,.05,9.08,.05s9.22,3.94,9.22,8.88V56.24c0,4.9-3.94,8.81-9.22,8.81S0,61.14,0,56.24V8.92Zm9.08,55.16c4.56,0,7.95-3.5,7.95-7.85V8.92c0-4.39-3.39-7.92-7.95-7.92S1.27,4.54,1.27,8.92V56.24c0,4.35,3.32,7.85,7.81,7.85ZM3.67,9.03c0-2.95,2.19-5.24,5.41-5.24s5.48,2.3,5.48,5.24V56.14c0,2.95-2.19,5.17-5.48,5.17s-5.41-2.23-5.41-5.17V9.03Zm5.41,51.33c2.54,0,4.22-1.82,4.22-4.22V9.03c0-2.4-1.68-4.28-4.22-4.28s-4.15,1.88-4.15,4.28V56.14c0,2.4,1.68,4.22,4.15,4.22Z"/>
+      <path class="cls-1" d="M19.03,17.4L28.97,1.3h4.97V13.69h3.43v4.31h-3.43v46.5h-4.87V18h-10.05v-.6Zm11.34-.39V63.51h2.27V17.02h3.43v-2.35h-3.43V2.28h-2.83l-9.1,14.74h9.66Zm-.14-11.66h.14V14.67h-5.67l5.53-9.31Zm-1.16,8.33v-4.48l-2.66,4.48h2.66Z"/>
+    </g>
+  </defs>
+  <defs>
+    <g id="zero-five" data-name="zero-five">
+      <path class="cls-1" d="M25.26,58.56c.98,1.54,2.35,1.89,3.47,1.89,1.89,0,3.22-1.19,3.22-3.01,0-.04,0-21.33,0-21.37,0-.03,0-.06,0-.09-.09-1.85-1.47-2.51-3.67-2.51h-6.13V1.3h13.9V5.92h-8.79V28.93h.84c5.08,0,8.71,2.21,8.75,7.06,0,.03,0,.06,0,.09,0,.04,0,21.32,0,21.36-.06,4.25-3.61,7.57-8.12,7.57-3.01,0-5.99-1.61-7.53-4.17l4.06-2.28Zm3.47,5.46c3.78,0,6.77-2.89,6.82-6.59,0-.03,0-21.33,0-21.36,0-.03,0-.06,0-.09-.04-4.18-3.14-6.08-7.46-6.08h-2.14V4.94h8.79V2.28h-11.31v30.22h5.36c2.99,0,4.37,1.29,4.44,3.49,0,.03,0,.06,0,.09,0,.03,0,21.33,0,21.37,0,2.42-1.85,3.99-4.52,3.99-1.23,0-2.7-.32-3.85-1.47l-2,1.19c1.44,1.86,3.57,2.87,5.85,2.87Z"/>
+      <path class="cls-1" d="M0,8.88C0,3.94,3.87,0,9.08,0s9.22,3.94,9.22,8.88V56.19c0,4.9-3.94,8.81-9.22,8.81S0,61.09,0,56.19V8.88Zm9.08,55.16c4.56,0,7.95-3.5,7.95-7.85V8.88c0-4.39-3.39-7.92-7.95-7.92S1.27,4.49,1.27,8.88V56.19c0,4.35,3.32,7.85,7.81,7.85ZM3.67,8.98c0-2.95,2.19-5.24,5.41-5.24s5.48,2.3,5.48,5.24V56.09c0,2.95-2.19,5.17-5.48,5.17s-5.41-2.23-5.41-5.17V8.98Zm5.41,51.33c2.54,0,4.22-1.82,4.22-4.22V8.98c0-2.4-1.68-4.28-4.22-4.28s-4.15,1.88-4.15,4.28V56.09c0,2.4,1.68,4.22,4.15,4.22Z"/>
+    </g>
+  </defs>
 </svg>`)
